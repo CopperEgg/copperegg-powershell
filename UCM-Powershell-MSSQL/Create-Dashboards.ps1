9f47734317f1282459b415b62b0f7b14c85850a3
Gagandeep singh <gagandeep.singh@joshtechnologygroup.com>
2016-11-22 15:47:26 +0530

    CE-2398 IIS and MSSQL should not create dashboard if already present (#14)

--- UCM-Powershell-MSSQL/Create-Dashboards.ps1
+++ UCM-Powershell-MSSQL/Create-Dashboards.ps1
@@ -9,10 +9,36 @@ $root = $PSScriptRoot
 #>
 function Create-Dashboard([string]$ApiServer, [string]$ApiKey, [string]$ServiceName, [string]$DashboardName)
 {
-  $Request = New-Object System.Net.WebClient
+
   $URI = "$ApiServer/v2/revealmetrics/dashboards"
   $AuthInfo = $ApiKey + ':U'
   $AuthObject = 'Basic ' + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($AuthInfo))
+
+  $Request = New-Object System.Net.WebClient
+  $Request.Headers.Add('Authorization', $AuthObject)
+  $Request.Headers.Add('Accept', 'application/json')
+  $Request.Headers.Add("User-Agent", "PowerShell")
+  $Request.Headers.Add('Content-Type', 'application/json')
+  [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+  [System.Net.ServicePointManager]::Expect100Continue = $false
+  try
+  {
+    $Result = $Request.DownloadString($URI)
+    $DataJson = $Result | ConvertFrom-Json
+    if($DataJson.name -contains $DashboardName){
+      Write-Log "Dashboard with same name already exists"
+      return
+    }
+  }
+  Catch [system.exception]
+  {
+    Write-Log "Exception in sending request for getting dashbords on Uptime Cloud Monitor"
+    Write-Log "Exception name => $($_.Exception.GetType().Name) - $($_.Exception.Message), at line number $($_.InvocationInfo.ScriptLineNumber)"
+    Write-Log "More information about error (if any) => $($error[0] | out-string)"
+    return
+  }
+
+  $Request = New-Object System.Net.WebClient
   $Request.Headers.Add('Authorization', $AuthObject)
   $Request.Headers.Add('Accept', '*/*')
   $Request.Headers.Add("User-Agent", "PowerShell")
